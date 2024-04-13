667f9fd7b8c6f2560c83390feb593a6cca6383ca
Jonathan Kazmaier <jonathankazmaier@Jonathans-MacBook-Air.local>
2024-04-13 16:27:48 -0500

    updated front end

--- frontend/name/src/App.js
+++ frontend/name/src/App.js
@@ -6,15 +6,26 @@ function App() {
   const response_list = response_boxes.map(response_box => 
     <div type="text" className="output_text" key={response_box.key}>{response_box.message}</div>
   )
+
   function submit(event){
     if (event.keyCode === 13) {
       sendText();
     }
   }
+
   function createAnswerBox(data) {
     data.key=response_boxes.length;
     set_response_boxes(response_boxes.concat(data));
+    document.getElementById("command-input").value = "";
+    scrollToBottom()
   }
+
+  function scrollToBottom() {
+    var cl = document.getElementById("command-line");
+    cl.scrollTop = cl.scrollHeight;
+    console.log(cl.scrollHeight)
+  }
+
   function sendText() {
     var inputText = document.getElementById("command-input").value;
     fetch('http://127.0.0.1:8080/calculate', {
@@ -37,15 +48,20 @@ function App() {
     .catch(error => {
         console.error('Error:', error);
     });
-}
+  }
+
   return (
     <main className="container">
+      <div className="top-screen">
+        <div className='logo'>ToBeDecided</div>
+        <div className='help-id'>Command 'help' for help</div>
+      </div>
       <div className="command-line" id="command-line">
       {
         response_list
       }
         <span className="blinking">&gt;</span>
-          <input type="text" id="command-input" autoFocus={true} autoCorrect='off' onBlur={({ target }) => target.focus()} autoComplete="off" className="input_text" onKeyDown={submit}></input>
+          <input type="text" id="command-input" autoFocus={true} autoCorrect='off' spellCheck='false' onBlur={({ target }) => target.focus()} autoComplete="off" className="input_text" onKeyDown={submit}></input>
       </div>
     </main>
   )
