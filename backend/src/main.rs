ad55ddb5ee5efd3d463c812d54f037aea4502289
Jonathan Kazmaier <jonathankazmaier@Jonathans-MacBook-Air.local>
2024-04-12 21:58:49 -0500

    rewrote backend to be CORS compliant

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,41 @@
-use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use actix_cors::Cors;
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Serialize;
 
-async fn index() -> impl Responder {
-    "Hello, world!"
-}
-
-async fn receive_string(payload: String) -> impl Responder {
-    println!("Received string: {}", payload);
-    HttpResponse::Ok().body("String received by the server.")
+#[derive(Serialize, Debug)]
+struct ResponseData {
+    message: String,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    fn receive_string(payload: String) -> Result<HttpResponse, actix_web::Error> {
+        println!("Received string: {}", payload);
+
+        // Creating a ResponseData struct and serializing it to JSON
+        let response_data = ResponseData {
+            message: String::from("String received by the server."),
+        };
+        Ok(HttpResponse::Ok().body(format!("go fuck yourself nigger {:?}", response_data)))
+    }
+
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(index))
-            .route("/send-string", web::post().to(receive_string))
+            .wrap(
+                Cors::default() // Allow all origins by default
+                    .allow_any_origin()
+                    .allowed_methods(vec!["GET", "POST"])
+                    .allowed_header("content-type")
+                    .max_age(3600), // Cache preflight OPTIONS requests for 1 hour
+            )
+            .route(
+                "/",
+                web::get().to(|| async { HttpResponse::Ok().body("Hello, world!") }),
+            )
+            .route(
+                "/calculate",
+                web::post().to(|payload: String| async move { receive_string(payload) }),
+            )
     })
     .bind("127.0.0.1:8080")?
     .run()
