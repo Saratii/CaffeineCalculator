6d6a210f80abac6b8a295a9856cd225bb717242d
Jonathan Kazmaier <jonathankazmaier@Jonathans-MacBook-Air.local>
2024-04-13 01:52:03 -0500

    ui

--- frontend/name/src/App.js
+++ frontend/name/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <div classname="test">
+      <h1>Test</h1>
     </div>
-  );
+  )
 }
 
 export default App;
