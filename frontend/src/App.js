580586dacc918d763ab228219466e010c6e57298
Sarati <friedchicken14821@gmail.com>
2024-04-14 04:50:23 -0500

    e

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,102 +4,97 @@ import React, { Component } from 'react';
 import Graph from './components/Graph/Graph';
 
 function App() {
- const [response_boxes, set_response_boxes] = useState([]);
-  const response_list = response_boxes.map(response_box =>
-   <div type="text" className="output_text" key={response_box.key}>{response_box.message}</div>
- );
+    const [response_boxes, set_response_boxes] = useState([]);
+    const [graph_points, set_graph_points] = useState([]);
+    const response_list = response_boxes.map(response_box =>
+        <div type="text" className="output_text" key={response_box.key}>{response_box.message}</div>
+    );
 
+    function submit(event){
+        if (event.keyCode === 13) {
+            sendText();
+        }
+    }
 
- function submit(event){
-   if (event.keyCode === 13) {
-     sendText();
-   }
- }
+    function createAnswerBox(data) {
+        if (data.message.indexOf("),(") > -1) {
+            set_graph_points(data.message);
+        } else {
+            data.key = response_boxes.length;
+            set_response_boxes(response_boxes.concat(data));
+            document.getElementById("command-input").value = "";
+            scrollToBottom();
+        }
+    }
 
- function createAnswerBox(data) {
-   data.key=response_boxes.length;
-   set_response_boxes(response_boxes.concat(data));
-   document.getElementById("command-input").value = "";
-   scrollToBottom()
- }
+    function scrollToBottom() {
+        var cl = document.getElementById("command-line");
+        cl.scrollTop = cl.scrollHeight;
+    }
 
+    function sendText() {
+        var inputText = document.getElementById("command-input").value;
+        fetch('http://127.0.0.1:8080/calculate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ text: inputText })
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Something went wrong');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log("Received response:", data);
+            createAnswerBox(data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 
- function scrollToBottom() {
-   var cl = document.getElementById("command-line");
-   cl.scrollTop = cl.scrollHeight;
- }
+    const maxX = graph_points.length > 0 ? graph_points.reduce((max, point) => {
+        return point.x > max ? point.x : max;
+    }, -Infinity) : 0;
 
+    const maxY = graph_points.length > 0 ? graph_points.reduce((max, point) => {
+        return point.y > max ? point.y : max;
+    }, -Infinity) : 0;
 
- function sendText() {
-   var inputText = document.getElementById("command-input").value;
-   fetch('http://127.0.0.1:8080/calculate', {
-     method: 'POST',
-     headers: {
-         'Content-Type': 'application/json',
-     },
-     body: JSON.stringify({ text: inputText })
-   })
-   .then(response => {
-     if (!response.ok) {
-       throw new Error('Something went wrong');
-     }
-     return response.json();
-   })
-   .then(data => {
-     console.log("Received response:", data);
-     createAnswerBox(data);
-   })
-   .catch(error => {
-       console.error('Error:', error);
-   });
- }
+    const minX = graph_points.length > 0 ? graph_points.reduce((min, point) => {
+        return point.x < min ? point.x : min;
+    }, Infinity) : 0;
 
+    const minY = graph_points.length > 0 ? graph_points.reduce((min, point) => {
+        return point.y < min ? point.y : min;
+    }, Infinity) : 0;
 
- const dataPoints = [{x:0, y:0}];
-
- const maxX = dataPoints.length > 0 ? dataPoints.reduce((max, point) => {
-  return point.x > max ? point.x : max;
- }, -Infinity) : 0;
-
- const maxY = dataPoints.length > 0 ? dataPoints.reduce((max, point) => {
-  return point.y > max ? point.y : max;
- }, -Infinity) : 0;
-
- const minX = dataPoints.length > 0 ? dataPoints.reduce((min, point) => {
-  return point.x < min ? point.x : min;
- }, Infinity) : 0;
-
- const minY = dataPoints.length > 0 ? dataPoints.reduce((min, point) => {
-  return point.y < min ? point.y : min;
- }, Infinity) : 0;
-
-
-
- return (
-   <main className="container">
-     <div className="top-screen">
-       <div className='logo'>CaffeineCalculator</div>
-       <div className='help-id'>Command 'help' for help</div>
-     </div>
-     <div className="blackbox">
-       <div className="command-line" id="command-line">
-         {
-           response_list
-         }
-         <span className="blinking">&gt;</span>
-         <input type="text" id="command-input" autoFocus={true} autoCorrect='off' spellCheck='false' onBlur={({ target }) => target.focus()} autoComplete="off" className="input_text" onKeyDown={submit}></input>
-       </div>
-       <Graph
-          minX={minX}
-          maxX={maxX}
-          minY={minY}
-          maxY={maxY}
-          dataPoints={dataPoints}></Graph>
-     </div>
-  
-   </main>
- )
+    return (
+        <main className="container">
+            <div className="top-screen">
+                <div className='logo'>CaffeineCalculator</div>
+                <div className='help-id'>Command 'help' for help</div>
+            </div>
+            <div className="blackbox">
+                <div className="command-line" id="command-line">
+                    {
+                        response_list
+                    }
+                    <span className="blinking">&gt;</span>
+                    <input type="text" id="command-input" autoFocus={true} autoCorrect='off' spellCheck='false' onBlur={({ target }) => target.focus()} autoComplete="off" className="input_text" onKeyDown={submit}></input>
+                </div>
+                <Graph
+                    minX={minX}
+                    maxX={maxX}
+                    minY={minY}
+                    maxY={maxY}
+                    dataPoints={graph_points}></Graph>
+            </div>
+        </main>
+    )
 }
-
 
 export default App;
