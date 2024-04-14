2ba799e08777ccc2523ac187c53d143ad88df717
Jonathan Kazmaier <jonathankazmaier@Jonathans-MacBook-Air.local>
2024-04-13 23:23:26 -0500

    ignore

--- frontend/name/src/components/Graph/Graph.js
+++ frontend/name/src/components/Graph/Graph.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
 import CanvasJSReact from '@canvasjs/react-charts';
 
+
 const CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-class Graph extends Component {
-	render() {
-		const options = {
-			theme: "dark2",
-			animationEnabled: true,
-			zoomEnabled: true,
-			title:{
-				text: "Placeholder"
-			},
-			axisX: {
-				title:"X",
-				crosshair: {
-					enabled: false,
-					snapToDataPoint: true
-				},
-                minimum: -10,
-                maximum: 10,
-			},
-			axisY:{
-				title: "Y",
-				crosshair: {
-					enabled: true,
-					snapToDataPoint: true
-				},
-                minimum: -10,
-                maximum: 10,
-                gridThickness: 0,
-			},
-			data: [{
-				type: "scatter",
-				markerSize: 1,
-				dataPoints: this.props.dataPoints
-			}]
-		};
 
-		return (
-			<div>
-				<CanvasJSChart options={options} />
-			</div>
-		);
-	}
+class Graph extends Component {
+   render() {
+       const options = {
+           theme: "dark2",
+           animationEnabled: true,
+           zoomEnabled: true,
+           axisX: {
+               crosshair: {
+                   enabled: false,
+                   snapToDataPoint: true
+               },
+               minimum: 0,
+               maximum: 10,
+               gridThickness: 0,
+           },
+           axisY:{
+               crosshair: {
+                   enabled: true,
+                   snapToDataPoint: true
+               },
+               minimum: 0,
+               maximum: 10,
+               gridThickness: 0,
+           },
+           data: [{
+               type: "line",
+               markerSize: 5,
+               dataPoints: this.props.dataPoints
+           }]
+       };
+
+
+       return (
+           <div style={{ width: '500px', height: '300px' }}>
+               <CanvasJSChart options={options} />
+           </div>
+       );
+   }
 }
+
 
 export default Graph;
